2f76911c600a2774d52cdfae9ec9c938fbc120c2
Marc Nygaard <33378153+Many5900@users.noreply.github.com>
2017-11-22 20:48:44 +0100

    v00.2

---
+++ js/version.js
@@ -0,0 +1,9 @@
+var v = "<p>Version: 00.2</p>";
+
+var l = "<p>Lavet den 20. november 2017.</p>";
+
+var s = "<p>Senest opdateret den 22. november 2017.</p>";
+
+document.write(v);
+document.write(l);
+document.write(s);
