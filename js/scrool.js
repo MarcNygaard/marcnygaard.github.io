b986f31adb845fcff69fb492009b3ae9392a819a
Marc Nygaard <33378153+Many5900@users.noreply.github.com>
2018-02-04 17:51:47 +0100

    V1

--- js/scrool.js
+++
@@ -1,9 +0,0 @@
-function top() {
-    document.getElementById( 'top' ).scrollIntoView();  
-    this.onclick();
-};
-
-function bottom() {
-    document.getElementById( 'bottom' ).scrollIntoView();
-    window.setTimeout( function () { top(); }, 2000 );
-};
--- js/version.js
+++
@@ -1,9 +0,0 @@
-var v = "<p>Version: 00.3</p>";
-
-var l = "<p>Lavet den 20. november 2017.</p>";
-
-var s = "<p>Senest opdateret den 23. november 2017.</p>";
-
-document.write(v);
-document.write(l);
-document.write(s);
